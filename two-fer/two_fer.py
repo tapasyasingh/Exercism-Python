f1889b87010823d460af3846752f1c7a5b909d39
tapasyasingh <tapasyabsingh97@gmail.com>
2019-04-04 17:45:15 +0530

    Correction 1

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -2,5 +2,5 @@ def two_fer(name=""):
     if name == "you": 
         return "One for you, one for me."
     else:
-        return  "One for "+ name ", one for me."
+        return  "One for "+ name + ", one for me."
     
